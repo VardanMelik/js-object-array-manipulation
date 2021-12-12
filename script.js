97140f77f6ba5a157a8c3f363e34c5e366976a46
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-12 23:12:37 +0400

    update

---
+++ script.js
@@ -0,0 +1,79 @@
+// Array.filter() filtering array by length size
+/*
+const words = [
+    'adidas',
+    'nike',
+    'aser',
+    'gagik',
+    'ashot',
+    'nunufar',
+    'nvidia',
+    'samsung',
+    'iphone',
+    'destruction',
+    'exuberant'
+];
+
+const result = words.filter( word => word.length > 6);
+
+console.log(result);
+*/
+/* ---------------------------------------------- */
+// Filtering out all small values - Does not work
+/*function isBigEnough(value) {
+    return value => 10;
+}
+
+let filtered = [12, 5, 8, 130, 44].filter(isBigEnough)
+console.log(filtered)
+*/
+
+/* ---------------------------------- */
+// Find all prime numbers in an array
+/*const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+    for (let i=2; num > i; i++) {
+        if (num % i ==0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+console.log(array.filter(isPrime));
+*/
+
+
+/* -------------------------------- */
+// Filtering invalid entries from JSON
+/*
+let arr = [
+    { id: 15 },
+    { id: -1 },
+    { id: 0 },
+    { id: 3 },
+    { id: 12.2 },
+    {  },
+    { id: null },
+    { id: NaN },
+    { id: 'undefined' }
+]
+
+let invalidEntries = 0;
+
+function filterByID(item) {
+    if (Number.isFinite(item.id) && item.id !==0) {
+        return true
+    } 
+    invalidEntries++
+    return false;
+}
+
+let arrByID = arr.filter(filterByID);
+
+console.log('Filtered Array\n', arrByID);
+console.log('Number of Invalid Entries = ', invalidEntries)
+*/
+
+/* Searching in array */
