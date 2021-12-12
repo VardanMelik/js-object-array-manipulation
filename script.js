16d1bd2690eb1869fa9a875b7f1d56fb1ad6db86
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-13 00:13:35 +0400

    update

--- script.js
+++ script.js
@@ -77,3 +77,13 @@ console.log('Number of Invalid Entries = ', invalidEntries)
 */
 
 /* Searching in array */
+let fruits = ['apple', 'banana', 'grapes', 'mango', 'orange']
+
+function filterItems(arr, query) {
+    return arr.filter( (el) => {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+console.log(filterItems(fruits, 'ap'));
+console.log(filterItems(fruits, 'an'))
---
+++ timer.js
@@ -0,0 +1,7 @@
+/*
+
+Affecting Initial Array
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+
+*/
